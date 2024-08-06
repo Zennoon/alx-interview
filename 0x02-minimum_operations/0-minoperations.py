b4f47c92ca9ba372dd301f02bc367237bc03f21d
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-08-06 23:36:37 +0300

    Write a function to solve the minimum operations task

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Contains:
+    Functions
+    =========
+    minOperations - Assuming we have one character in a file
+    and we can perform two operation: copy all, and paste, the
+    function returns the minimum number of operations it would take
+    to reach a given number of characters
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    Assuming we have one character in a file
+    and we can perform two operation: copy all, and paste, the
+    function returns the minimum number of operations it would take
+    to reach a given number of characters
+    """
+    if n < 1:
+        return 0
+    prime_factors = {}
+    for i in range(2, n + 1):
+        while n % i == 0:
+            if i in prime_factors:
+                prime_factors[i] += 1
+            else:
+                prime_factors[i] = 1
+            n /= i
+        if n <= 1:
+            break
+    return (sum([key * val for key, val in prime_factors.items()]))
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,2 @@
+This directory was created on August 6, 2024 for the ALX 
+interview prep project 'Minimum Operations'
