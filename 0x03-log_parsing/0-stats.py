43de4e2c14f7ad0ce387bb3ee7f8bee4b62d6638
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-08-15 15:43:13 +0000

    Try inspired code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,66 +1,72 @@
 #!/usr/bin/python3
-"""
-Contains:
-    Global
-    ======
-    Script to read and parse input and display related statistics
-    every 10 lines read or when CTRL+C is clicked
-"""
 import re
-import signal
-import sys
 
 
-def print_stats(total_f_size, status_log):
-    """Displays info about stats recorded until calling time"""
-    print("File size: {}".format(total_f_size), flush=True)
-    sys.stdout.flush()
-    for stat_code, freq in sorted([*status_log.items()]):
-        print("{}: {}".format(stat_code, freq), flush=True)
+def extract_input(input_line):
+    fp = (
+        r'\s*(?P<ip>\S+)\s*',
+        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+        r'\s*"(?P<request>[^"]*)"\s*',
+        r'\s*(?P<status_code>\S+)',
+        r'\s*(?P<file_size>\d+)'
+    )
+    info = {
+        'status_code': 0,
+        'file_size': 0,
+    }
+    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
+    resp_match = re.fullmatch(log_fmt, input_line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        info['status_code'] = status_code
+        info['file_size'] = file_size
+    return info
 
 
-def validate_line(line, pattern):
-    """
-    Checks that the line is of the correct format
-    """
-    return pattern.match(line)
+def print_statistics(total_file_size, status_codes_stats):
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
+
+
+def update_metrics(line, total_file_size, status_codes_stats):
+    line_info = extract_input(line)
+    status_code = line_info.get('status_code', '0')
+    if status_code in status_codes_stats.keys():
+        status_codes_stats[status_code] += 1
+    return total_file_size + line_info['file_size']
 
 
 def run():
-    """Main function"""
-    counter = 0
-    status_log = dict()
-    total_f_size = 0
-    pattern = re.compile(
-        r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} - \[.*\] "
-        + r"\"GET /projects/260 HTTP/1.1\" ([0-9]{3}) ([0-9]+)"
-    )
-    possible_codes = ('200', '301', '400', '401', '403', '404', '405', '500')
-    def handler(signum, frame):
-        """Handles CTRL+C interruption"""
-        print_stats(total_f_size, status_log)
-        raise KeyboardInterrupt()
+    line_num = 0
+    total_file_size = 0
+    status_codes_stats = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0,
+    }
+    try:
+        while True:
+            line = input()
+            total_file_size = update_metrics(
+                line,
+                total_file_size,
+                status_codes_stats,
+            )
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics(total_file_size, status_codes_stats)
+    except (KeyboardInterrupt, EOFError):
+        print_statistics(total_file_size, status_codes_stats)
 
 
-    signal.signal(signal.SIGINT, handler)
-    for line in sys.stdin:
-        counter += 1
-        matched = validate_line(line.strip(), pattern)
-        if matched:
-            stat, f_size = matched[1], matched[2]
-            try:
-                if stat in possible_codes:
-                    if stat in status_log:
-                        status_log[stat] += 1
-                    else:
-                        status_log[stat] = 1
-            except Exception as e:
-                print(e)
-            else:
-                total_f_size += int(f_size)
-        if counter % 10 == 0:
-            print_stats(total_f_size, status_log)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     run()
