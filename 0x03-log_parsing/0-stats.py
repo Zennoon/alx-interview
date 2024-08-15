231c99900ceb019fd646dc2463dbf662d41861d9
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-08-15 15:41:02 +0000

    Try new approach

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -11,53 +11,56 @@ import signal
 import sys
 
 
-def handler(signum, frame):
-    """Handles CTRL+C interruption"""
-    print_stats()
-    raise KeyboardInterrupt()
-
-
-signal.signal(signal.SIGINT, handler)
-
-counter = 0
-status_log = dict()
-total_f_size = 0
-pattern = re.compile(
-    r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} - \[.*\] "
-    + r"\"GET /projects/260 HTTP/1.1\" ([0-9]{3}) ([0-9]+)"
-)
-
-
-def print_stats():
+def print_stats(total_f_size, status_log):
     """Displays info about stats recorded until calling time"""
-    sys.stdout.write("File size: {}\n".format(total_f_size))
+    print("File size: {}".format(total_f_size), flush=True)
     sys.stdout.flush()
     for stat_code, freq in sorted([*status_log.items()]):
-        sys.stdout.write("{}: {}\n".format(stat_code, freq))
-        sys.stdout.flush()
+        print("{}: {}".format(stat_code, freq), flush=True)
 
 
-def validate_line(line):
+def validate_line(line, pattern):
     """
     Checks that the line is of the correct format
     """
     return pattern.match(line)
 
 
-for line in sys.stdin:
-    # counter += 1
-    matched = validate_line(line.strip())
-    if matched:
+def run():
+    """Main function"""
+    counter = 0
+    status_log = dict()
+    total_f_size = 0
+    pattern = re.compile(
+        r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3} - \[.*\] "
+        + r"\"GET /projects/260 HTTP/1.1\" ([0-9]{3}) ([0-9]+)"
+    )
+    possible_codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    def handler(signum, frame):
+        """Handles CTRL+C interruption"""
+        print_stats(total_f_size, status_log)
+        raise KeyboardInterrupt()
+
+
+    signal.signal(signal.SIGINT, handler)
+    for line in sys.stdin:
         counter += 1
-        stat, f_size = matched[1], matched[2]
-        try:
-            if int(stat) in status_log:
-                status_log[int(stat)] += 1
+        matched = validate_line(line.strip(), pattern)
+        if matched:
+            stat, f_size = matched[1], matched[2]
+            try:
+                if stat in possible_codes:
+                    if stat in status_log:
+                        status_log[stat] += 1
+                    else:
+                        status_log[stat] = 1
+            except Exception as e:
+                print(e)
             else:
-                status_log[int(stat)] = 1
-        except Exception:
-            pass
-        else:
-            total_f_size += int(f_size)
+                total_f_size += int(f_size)
         if counter % 10 == 0:
-            print_stats()
+            print_stats(total_f_size, status_log)
+
+
+if __name__ == "__main__":
+    run()
