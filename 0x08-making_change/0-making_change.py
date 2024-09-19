45a21e9d398ba4b3eca738fe1d3140319d9d881f
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-19 18:58:44 +0300

    Try a trivial and inaccurate solution

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -5,21 +5,30 @@ Contains:
     =========
     makeChange - function to solve the make change problem
 """
-
-
 def makeChange(coins, total):
-    """Function to solve the make change problem"""
+    """
+    Solution for the make change problem
+    """
     if total <= 0:
         return 0
-    coins.sort()
-    all_nums = {i: total+1 for i in range(total + 1)}
-    all_nums[0] = 0
-    for i in range(1, len(all_nums)):
-        for coin in coins:
-            if i - coin < 0:
-                break
+    if total in coins:
+        return 1
+
+    coins.sort(reverse=True)
+    counter = 0
+    for i in coins:
+        if total % i == 0:
+            counter += int(total / i)
+            return counter
+        if total - i >= 0:
+            if total / i > 1:
+                counter += int(total / i)
+                total = total % i
             else:
-                all_nums[i] = min(all_nums[i], 1 + all_nums[i - coin])
-    if all_nums[total] > total:
+                counter += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
         return -1
-    return all_nums[total]
+    return counter
