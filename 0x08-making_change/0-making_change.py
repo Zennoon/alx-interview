56dca561546201e837d7437391a73b4f2811d03a
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-19 17:03:30 +0300

    Introduce slight improvement in performance by sorting coins array

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -11,15 +11,15 @@ def makeChange(coins, total):
     """Function to solve the make change problem"""
     if total <= 0:
         return 0
-    all_nums = {i:total+1 for i in range(total + 1)}
+    coins.sort()
+    all_nums = {i: total+1 for i in range(total + 1)}
     all_nums[0] = 0
     for i in range(1, len(all_nums)):
         for coin in coins:
             if i - coin < 0:
-                all_nums[i] = min(all_nums[i], total + 1)
+                break
             else:
                 all_nums[i] = min(all_nums[i], 1 + all_nums[i - coin])
     if all_nums[total] > total:
         return -1
     return all_nums[total]
-
