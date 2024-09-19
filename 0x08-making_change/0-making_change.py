a7ef71fef821175057d9b8037bd87fee1ea01a55
Zennoon <98653096+Zennoon@users.noreply.github.com>
2024-09-19 15:44:24 +0300

    Write a function to solve the make change problem

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Contains:
+    Functions
+    =========
+    makeChange - function to solve the make change problem
+"""
+
+
+def makeChange(coins, total):
+    """Function to solve the make change problem"""
+    if total <= 0:
+        return 0
+    all_nums = [total + 1 for n in range(total + 1)]
+    all_nums[0] = 0
+    for i in range(1, len(all_nums)):
+        for coin in coins:
+            if i - coin < 0:
+                all_nums[i] = min(all_nums[i], total + 1)
+            else:
+                all_nums[i] = min(all_nums[i], 1 + all_nums[i - coin])
+    if all_nums[-1] > total:
+        return -1
+    return all_nums[-1]
---
+++ 0x08-making_change/README.md
@@ -0,0 +1,2 @@
+This directory was created on September 19, 2024 for the ALX SE interview 
+prep project '0x08. Making Change'.
